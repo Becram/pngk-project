018b3e30ecda0d52af27963cda4e49519ede34a4
Becram <itbecram@gmail.com>
2019-03-22 11:55:04 +0545

    added scripts

--- provison.sh
+++ provison.sh
@@ -15,15 +15,18 @@ if (( ${#missing_deps[@]} > 0 )) ; then
     On Debian/Ubuntu try 'sudo apt install kpartx qemu-user-static parted wget curl jq aria2'"
 fi
 
+
+
 function run_scripts {
-         run-parts --exit-on-error -v --regex '[a-zA-Z.-_]*'  ./scripts.d
-         retVal=$?
-         if [ $retVal -ne 0 ]; then
-             log error "Something went wrong please check your script"
-             exit $retVal
-         else
-             log notice "Sucessfully configured"
-         fi
+    run-parts --exit-on-error -v --regex '[a-zA-Z.-_]*'  ./scripts.d
+    retVal=$?
+    if [ $retVal -ne 0 ]; then
+        log error "Something went wrong please check your script"
+        exit $retVal
+    else
+        log notice "Sucessfully configured"
+    fi
 }
+
 
 run_scripts
